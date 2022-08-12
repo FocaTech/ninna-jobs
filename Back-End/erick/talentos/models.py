52f005131d3a4828725afb4002356eeae9d8be3e
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2022-08-11 21:41:05 -0300

    Início da reestruturação do projeto

--- Back-End/erick/talentos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from datetime import datetime
-
-class Cadastro(models.Model):
-    nome_cad = models.CharField(max_length=200)
-    email_cad = models.CharField(max_length=200)
-    data_criacao = models.DateField(datetime)
-
-
--- Back-End/erick/talentos/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import get_list_or_404, render
-#from .models import Cadastro
-
-def cadastro(request):
-    return render(request, 'cadastro.html')
-
-# Create your views here.
