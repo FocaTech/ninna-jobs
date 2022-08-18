41e4627836e210c807b3b87be8bec49d920ab8d1
Kauhê Victo dos Santos Silva <103337542+Kauhe-Victor@users.noreply.github.com>
2022-08-18 19:07:54 -0300

    Mostrando logo da empresa na index

--- ninna_jobs/settings.py
+++ ninna_jobs/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS=[
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
 
+#media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
