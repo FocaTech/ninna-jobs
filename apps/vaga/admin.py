b05d3b460c72cb05f88a68e853aad23ab57a1310
Kauhê Victo dos Santos Silva <103337542+Kauhe-Victor@users.noreply.github.com>
2022-08-17 21:10:16 -0300

    Preparando as migrações e registrando no admin

--- apps/vaga/admin.py
+++ apps/vaga/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vagas, TipoContratacao, TipoTrabalho, PerfilProfissional
+
+admin.site.register(Vagas)
+admin.site.register(TipoContratacao)
+admin.site.register(TipoTrabalho)
+admin.site.register(PerfilProfissional)
