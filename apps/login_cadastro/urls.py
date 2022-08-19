544e09149e6e188941a50c7abea85b5271d668bb
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2022-08-19 13:28:51 -0300

    Removendo vírgula extra que tava crashando o servidor

--- apps/login_cadastro/urls.py
+++ apps/login_cadastro/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('candidatos', views.cadastro_candidatos, name='cadastro_candidatos'),
-    path('empresas', views.cadastro_empresas, name='cadastro_empresas'),,
+    path('empresas', views.cadastro_empresas, name='cadastro_empresas'),
 
 
     # para testar as partials
