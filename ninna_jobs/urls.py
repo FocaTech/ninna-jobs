ffe08b284befea6b5d4019e63468025fbfdc43cc
Hemerson-Rocha <hemerson.me@gmail.com>
2022-09-20 20:50:09 -0300

    correçoes

--- ninna_jobs/urls.py
+++ ninna_jobs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('conta/', include("login_cadastro.urls")),
     path('vaga/', include("vaga.urls")),
+    path('', include("vaga.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
