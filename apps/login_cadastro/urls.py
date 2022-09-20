becd7ec194c6a77eb869dc462ed8cca3a2e2b743
ErickPTCosta <erick.lawliet@gmail.com>
2022-09-20 19:16:56 -0300

    pequenas alterações das urls

--- apps/login_cadastro/urls.py
+++ apps/login_cadastro/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/Candidato/', views.longar_candidato, name='longar_candidato'),
-    path('login/Empresa/', views.longar_empresa, name='longar_empresa'),
+    path('login/candidato/', views.longar_candidato, name='longar_candidato'),
+    path('login/empresa/', views.longar_empresa, name='longar_empresa'),
     path('cadastro/candidato/', views.cadastro_candidatos, name='cadastro_candidatos'),
     path('cadastro/empresas/', views.cadastro_empresas, name='cadastro_empresas'),
 
