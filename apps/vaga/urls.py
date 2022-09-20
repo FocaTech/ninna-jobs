01da13e1d0890d63e1933f3a2f060e8884274719
Kauhê Victo dos Santos Silva <103337542+Kauhe-Victor@users.noreply.github.com>
2022-09-20 19:40:43 -0300

    Começo da implementação do cadastro de vagas

--- apps/vaga/urls.py
+++ apps/vaga/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('perfil', views.perfil, name='perfil'),
-    path('404', views.not_found, name='not_found')
+    path('404', views.not_found, name='not_found'),
+    path('conta/empresa', views.select, name='select'),
 ]
--- apps/vaga/views.py
+++ apps/vaga/views.py
@@ -2,6 +2,24 @@ from pickletools import read_uint8
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import TipoContratacao, TipoTrabalho, Vagas, PerfilProfissional
 
+def select(request):
+    contratacoes = TipoContratacao.objects.all()
+    trabalhos = TipoTrabalho.objects.all()
+    perfis = PerfilProfissional.objects.all()
+
+
+    dado = {
+        'contratacoes' : contratacoes,
+        'trabalhos' : trabalhos,
+        'perfis' : perfis,
+    }
+
+    return render(request, 'empresa.html', dado)
+
+
+
+
+
 def index(request):
     vagas = Vagas.objects.all()
 
