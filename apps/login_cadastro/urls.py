4990f0c44053fff420e1dac618d8e24521d759ca
ErickPTCosta <erick.lawliet@gmail.com>
2022-09-20 18:58:49 -0300

    Correção Urls estão funcionando

--- apps/login_cadastro/urls.py
+++ apps/login_cadastro/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('login/Candidato/', views.longar_candidato, name='longar_candidato'),
     path('login/Empresa/', views.longar_empresa, name='longar_empresa'),
-    path('SignUp/Candidato/', views.cadastro_candidatos, name='cadastro_candidatos'),
-    path('Sign/Up/empresas/', views.cadastro_empresas, name='cadastro_empresas'),
+    path('cadastro/candidato/', views.cadastro_candidatos, name='cadastro_candidatos'),
+    path('cadastro/empresas/', views.cadastro_empresas, name='cadastro_empresas'),
 
 
     # para testar as partials
