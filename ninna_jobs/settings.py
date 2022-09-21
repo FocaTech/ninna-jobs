2ec476a1d18bc46bb618585a71a7afffa5ed75c2
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2022-09-20 21:42:03 -0300

    pasta static movida pra raiz do projeto

--- ninna_jobs/settings.py
+++ ninna_jobs/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'ninna_jobs/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
