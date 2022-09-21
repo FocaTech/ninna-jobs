b59c09a6c968e14800f92645d2288f21aa5390b2
AliceKowai <devalice117@gmail.com>
2022-09-21 17:13:18 -0300

    desespero

--- apps/vaga/urls.py
+++ apps/vaga/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('perfil', views.perfil, name='perfil'),
+    path('perfilempresa', views.perfilempresa, name='perfilempresa'),
     path('404', views.not_found, name='not_found'),
     path('empresa', views.select, name='select'),
-    #path('', views.vagas, name='vagas'),
+    #path('vagas', views.vagas, name='vagas'),
+    path('talentos', views.talentos, name='talentos'),
 ]
