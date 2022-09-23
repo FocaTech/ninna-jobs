c96779fcc0f66742d7616568bd948220366d0292
Hemerson-Rocha <hemerson.me@gmail.com>
2022-09-23 10:35:09 -0300

    correções

--- apps/login_cadastro/urls.py
+++ apps/login_cadastro/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/candidato/', views.longar_candidato, name='longar_candidato'),
-    path('login/empresa/', views.longar_empresa, name='longar_empresa'),
+    path('login/candidato/', views.logar_candidato, name='longar_candidato'),
+    path('login/empresa/', views.logar_empresa, name='longar_empresa'),
     path('cadastro/candidato/', views.cadastro_candidato, name='cadastro_candidato'),
     path('cadastro/empresas/', views.cadastro_empresa, name='cadastro_empresa'),
 
