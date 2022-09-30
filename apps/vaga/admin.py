12f9c06994d14f57e4d8257ab3809e54c930eada
Hemerson-Rocha <hemerson.me@gmail.com>
2022-09-30 10:57:46 -0300

    consertando erro

--- apps/vaga/admin.py
+++ apps/vaga/admin.py
@@ -8,8 +8,16 @@ class ListandoVagas(admin.ModelAdmin):
     search_fields = ('nome_vaga',)
     list_per_page = 10
 
+class ListandoVagasSalvas(admin.ModelAdmin):
+    list_display = ('id', 'id_cadidato', 'id_vaga')
+    list_display_links = ('id', 'id_cadidato', 'id_vaga')
+    search_fields = ('nome_vaga',)
+    list_per_page = 10
+
+admin.site.register(VagasSalvas, ListandoVagasSalvas)
+
+
 admin.site.register(Vagas, ListandoVagas)
 admin.site.register(TipoContratacao)
 admin.site.register(TipoTrabalho)
 admin.site.register(PerfilProfissional)
-admin.site.register(VagasSalvas)
