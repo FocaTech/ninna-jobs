c42f8dbd7edcf1d9a6586eaa39975d7706790853
Hemerson-Rocha <hemerson.me@gmail.com>
2022-10-11 22:13:42 -0300

    iniciando o candidatar a vaga

--- apps/vaga/admin.py
+++ apps/vaga/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from .models import Vagas, TipoContratacao, TipoTrabalho, PerfilProfissional, VagasSalvas
+from .models import Vagas, TipoContratacao, TipoTrabalho, PerfilProfissional, VagasSalvas, VagasCandidatadas
 
 class ListandoVagas(admin.ModelAdmin):
     list_display = ('id', 'nome_vaga', 'nome_empresa', 'local_empresa', )
     list_display_links = ('id', 'nome_vaga')
     search_fields = ('nome_vaga',)
     list_per_page = 10
+
+admin.site.register(Vagas, ListandoVagas)
 
 class ListandoVagasSalvas(admin.ModelAdmin):
     list_display = ('id', 'id_cadidato', 'id_vaga')
@@ -16,8 +18,16 @@ class ListandoVagasSalvas(admin.ModelAdmin):
 
 admin.site.register(VagasSalvas, ListandoVagasSalvas)
 
+class ListandoVagasCandidatadas(admin.ModelAdmin):
+    list_display = ('id', 'id_cadidato', 'id_vaga')
+    list_display_links = ('id', 'id_cadidato', 'id_vaga')
+    search_fields = ('nome_vaga',)
+    list_per_page = 10
 
-admin.site.register(Vagas, ListandoVagas)
+admin.site.register(VagasCandidatadas, ListandoVagasCandidatadas)
+
+
+
 admin.site.register(TipoContratacao)
 admin.site.register(TipoTrabalho)
 admin.site.register(PerfilProfissional)
