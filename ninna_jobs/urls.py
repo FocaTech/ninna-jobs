b4c58d3647cac0e22acbe2fa56cf781c0dd14c08
danilozzz <455bcgkaa@gmail.com>
2022-10-19 01:08:42 -0300

    k

--- ninna_jobs/urls.py
+++ ninna_jobs/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
     path('vaga/', include("vaga.urls")),
     path('', include("vaga.urls")),
     path('usuarios/', include("usuarios.urls")),
-    path('ninna/', include("admin.urls"))
+    path('ninna/', include("admin.urls")),
+    url(r'^', TemplateResponse, {'template': '404.html'})
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns.append(url(r'^', TemplateResponse, {'template': '404.html'}))
