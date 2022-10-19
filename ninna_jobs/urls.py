dbaa781cc80e0ea38780c3e0db3862ff45538eec
danilozzz <455bcgkaa@gmail.com>
2022-10-18 21:44:11 -0300

    mexendo na barra de busca

--- ninna_jobs/urls.py
+++ ninna_jobs/urls.py
@@ -9,7 +9,7 @@ from django.template.response import TemplateResponse
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), #debug false
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), #debug fale
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/img/favicon.ico')), #debug false
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')), #debug false
     path('admin/', admin.site.urls),
     path('conta/', include("login_cadastro.urls")),
     path('vaga/', include("vaga.urls")),
