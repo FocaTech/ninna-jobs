7b2b14f981e96f2a117829945b5be69ea8844a01
Kauhê Victo dos Santos Silva <103337542+Kauhe-Victor@users.noreply.github.com>
2022-10-26 19:29:34 -0300

    Alterações no novo formulário

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('formulario/empresa/', views.registro, name='registro'),
+    path('cadastrar/candidato/', views.cadastro_candidato_2, name='cadastro_candidato_2'),
 ]
