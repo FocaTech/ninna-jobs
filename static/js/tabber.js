f304254d023845956b85e01b8a5ff9291cb81704
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2022-10-26 23:40:02 -0300

    abas agora podem ser linkadas
    ainda não funciona se o link direciona pra página atual, mas já é alguma coisa

--- static/js/tabber.js
+++ static/js/tabber.js
@@ -1,9 +1,24 @@
-document.querySelectorAll('.tabber__tab').forEach(tab => {
+(function() {
+  function changeTab(tab, tabName) {
+    document.querySelector('.tabber__tab.active').classList.remove('active');
+    tab.classList.add('active');
+    document.querySelector('.tabber__content>.active').classList.remove('active');
+    document.querySelector('.tabber__content>#'+tabName).classList.add('active');
+  }
+
+  document.querySelectorAll('.tabber__tab').forEach(tab => {
     tab.onclick = () => {
-        const tabName = tab.innerText.toLowerCase().replaceAll(' ', '-');
-        document.querySelector('.tabber__tab.active').classList.remove('active');
-        tab.classList.add('active');
-        document.querySelector('.tabber__content>.active').classList.remove('active');
-        document.querySelector('.tabber__content>#'+tabName).classList.add('active');
+      const tabName = tab.innerText.toLowerCase().replaceAll(' ', '-');
+      changeTab(tab, tabName);
     }
-});
+  });
+
+  if (location.hash != '' && !!document.querySelector('.tabber')) {
+    document.querySelectorAll('.tabber__tab').forEach(tab => {
+      const tabName = tab.innerText.toLowerCase().replaceAll(' ', '-');
+      if (tabName == location.hash.slice(1)) {
+        changeTab(tab, tabName);
+      }
+    });
+  }
+})();
