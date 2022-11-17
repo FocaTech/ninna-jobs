724aba6a97de3c3d32f5b1d3e44ad4e9cb42e315
danilozzz <455bcgkaa@gmail.com>
2022-11-17 13:33:52 -0300

    melhorando

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('Ninna/Empresa/<int:pk_chat>', views.chat_empresa, name='chatE'),
     path('Ninna/Candidato/<int:pk_chat>', views.chat_candidato, name='chatC'),
-    path('teste/', views.mensagens, name='mensagens'),
+    path('Candidato/Mensagens/', views.mensagensC, name='mensagensC'),
+    path('Empresa/Mensagens/', views.mensagensE, name='mensagensE'),
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,14 +1,61 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from login_cadastro.models import Users
 from chat.models import Chat
-from usuarios.models import Dados_Pessoais
+from usuarios.models import Certificados_Conquistas, City, Dados_Pessoais, Empresa, Idiomas, Interesses, Experiência_Profissional, Informações_Iniciais, Formacao_Academica
+from vaga.models import Vagas, VagasCandidatadas, VagasSalvas, TipoContratacao, TipoTrabalho, PerfilProfissional
 
 
+
+
+def mensagensE(request):
+    '''lista quem mandou mensagem para o Candidato'''
+    msg = Chat.objects.order_by('-data_mensagem')
+    lista = []
+    for m in msg:
+        if m.mandou.pk != request.user.id:
+            lista.append(m)
+    lista2 = []
+    nome = ''
+    for l in lista:
+        if l.mandou.username != nome:
+            lista2.append(l)
+            nome = l.mandou.username
+            Dados = Dados_Pessoais.objects.filter(user=l.mandou)
+
+    dados = {
+        'mensagens':lista2,
+        'dados':Dados,
+    }
+    return render(request, 'chat/mensagemE.html', dados)
+
+def mensagensC(request):
+    '''lista quem mandou mensagem para o Candidato'''
+    id = request.user.id
+    msg = Chat.objects.order_by('-data_mensagem')
+    Dados = Dados_Pessoais.objects.order_by().filter(user=id)
+    DP = Dados_Pessoais.objects.order_by().filter(user=id)
+    lista = []
+    for m in msg:
+        if m.mandou.pk != request.user.id:
+            lista.append(m)
+    lista2 = []
+    nome = ''
+    for l in lista:
+        if l.mandou.username != nome:
+            lista2.append(l)
+            nome = l.mandou.username
+    dados = {
+        'Dados':DP,
+        'mensagens':lista2,
+        'dados':Dados,
+    }
+    return render(request, 'chat/mensagemC.html', dados)
 
 def chat_empresa(request, pk_chat):
     '''Mensagens da Empresa'''
     msg = Chat.objects.order_by('data_mensagem')
-    dados_can = get_object_or_404(Dados_Pessoais, user=pk_chat)
+    if len(Dados_Pessoais.objects.filter(user=pk_chat)) > 0:
+        dados_can = get_object_or_404(Dados_Pessoais, user=pk_chat)
     empresa = get_object_or_404(Users, pk=request.user.id)
     if request.method == 'POST':
         nome_empresa = empresa.username
@@ -25,7 +72,8 @@ def chat_empresa(request, pk_chat):
 
 def chat_candidato(request, pk_chat):
     msg = Chat.objects.order_by('data_mensagem')
-    dados_can = get_object_or_404(Dados_Pessoais, user=request.user.id)
+    if len(Dados_Pessoais.objects.filter(user=request.user.id)) > 0:
+        dados_can = get_object_or_404(Dados_Pessoais, user=request.user.id)
     empresa = get_object_or_404(Users, id=pk_chat)
     if request.method == 'POST':
         nome_empresa = empresa.username
@@ -39,13 +87,3 @@ def chat_candidato(request, pk_chat):
         'dados':dados_can
     }
     return render(request, 'chat/chatC.html', dados)
-
-def mensagens(request):
-    '''lista quem mandou mensagem para o Candidato'''
-    Dados = get_object_or_404(Dados_Pessoais, user=request.user.id)
-    msg = Chat.objects.order_by('-data_mensagem')
-    dados = {
-        'mensagens':msg,
-        'dados':Dados,
-    }
-    return render(request, 'chat/mensagem.html', dados)
